813ba0859c11219a3d19eb0386863ddcd143a51f
pragati411 <pragatishukla411@gmail.com>
2023-05-08 11:01:23 +0530

    certificate generator

---
+++ vite-project/src/App.jsx
@@ -0,0 +1,94 @@
+import { useState } from "react";
+import axios from "axios";
+import "dotenv";
+
+import "./App.css";
+import Header from "./Header/HeaderFile";
+import Form from "./Form/Index";
+import Preview from "./Preview";
+function App() {
+  const [formData, setFormData] = useState({
+    name: "",
+    course: "",
+  });
+  const [isLoading, setIsLoading] = useState(false);
+  const [certificate, setCertificate] = useState(null);
+
+  function generateCertificate(e) {
+    e.preventDefault();
+
+    setIsLoading(true);
+
+    const url = process.env.REACT_APP_MAKE_URL;
+
+    const headers = {
+      "Content-Type": "application/json",
+      "X-MAKE-API-KEY": process.env.REACT_APP_MAKE_KEY,
+    };
+
+    const data = {
+      size: "A4",
+      format: "pdf",
+      data: {
+        ...formData,
+        date: new Date().toDateString().split(" ").slice(1).join(" "),
+      },
+      postProcessing: {
+        optimize: true,
+      },
+    };
+
+    axios
+      .post(url, data, {
+        headers: headers,
+      })
+      .then(
+        (response) => {
+          setIsLoading(false);
+          setCertificate(response.data.resultUrl);
+        },
+        (error) => {
+          console.log(error);
+          setIsLoading(false);
+        }
+      );
+  }
+
+  return (
+    <div className="App">
+      <div className="container">
+        <Header />
+
+        <section>
+          <div>
+            <Form formData={formData} setFormData={setFormData} />
+            <button
+              type="button"
+              disabled={isLoading}
+              onClick={generateCertificate}
+            >
+              {isLoading ? "Making..." : "Make my certificate"}
+            </button>
+          </div>
+          <div>
+            <Preview certificate={certificate} isLoading={isLoading} />
+            {certificate && (
+              <a
+                className="download"
+                target="_blank"
+                rel="noreferrer"
+                href={certificate}
+              >
+                Download
+              </a>
+            )}
+          </div>
+        </section>
+
+        <footer>Built with React and Make.cm</footer>
+      </div>
+    </div>
+  );
+}
+
+export default App;
